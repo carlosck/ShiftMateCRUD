0bdcff51c058182c97a5eefa050a55ce466bdf62
carlosck <carlosck@gmail.com>
2023-06-17 16:24:54 -0600

    add sluglify

--- pages/api/create_project.tsx
+++ pages/api/create_project.tsx
@@ -10,6 +10,21 @@ import ReturnInfo from '../../helpers/returnInfo'
    ]
    
 } */
+function slugify(string) {
+    const a = 'àáâäæãåāăąçćčđďèéêëēėęěğǵḧîïíīįìıİłḿñńǹňôöòóœøōõőṕŕřßśšşșťțûüùúūǘůűųẃẍÿýžźż·/_,:;'
+    const b = 'aaaaaaaaaacccddeeeeeeeegghiiiiiiiilmnnnnoooooooooprrsssssttuuuuuuuuuwxyyzzz------'
+    const p = new RegExp(a.split('').join('|'), 'g')
+  
+    return string.toString().toLowerCase()
+      .replace(/\s+/g, '-') // Replace spaces with -
+      .replace(p, c => b.charAt(a.indexOf(c))) // Replace special characters
+      .replace(/&/g, '-and-') // Replace & with 'and'
+      .replace(/[^\w\-]+/g, '') // Remove all non-word characters
+      .replace(/\-\-+/g, '-') // Replace multiple - with single -
+      .replace(/^-+/, '') // Trim - from start of text
+      .replace(/-+$/, '') // Trim - from end of text
+  }
+
 export default async function handler(
     request: NextApiRequest,
     response: NextApiResponse
@@ -20,7 +35,7 @@ export default async function handler(
         const db= await getFirebaseAdmin();
         
         try{
-            await db.collection('shifts').doc(request.body.mail).collection('projects').doc(request.body.name).set({
+            await db.collection('shifts').doc(request.body.mail).collection('projects').doc(slugify(request.body.name)).set({
                     
                 name: request.body.name,
                 actors: request.body.actors,
